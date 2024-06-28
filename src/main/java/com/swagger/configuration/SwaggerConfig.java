0a196e1b5821808ecc164d38d650cfa6ac36525a
ChristianIlachoque <cilachoque@unsa.edu.pe>
2024-06-28 09:50:22 -0500

    Swagger implementation with OpenApi Documentation

---
+++ src/main/java/com/swagger/configuration/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.swagger.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpHeaders;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API CLUBES",
+                description = "Our app provides a concise listing of football team names",
+                termsOfService = "https://chrisilasdev.netlify/terminos_y_condiciones",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Christian Ilachoque",
+                        url = "https://chrisilasdev.netlify",
+                        email = "christian.ilachoque.dev@mail.com"
+                ),
+                license = @License(
+                        name = "Standard Software Use License for ChrisIlasDev",
+                        url = "https://chrisilasdev.netlify/licence"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "DEV SERVER",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD SERVER",
+                        url = "http://unprogramadornace:8080"
+                )
+        },
+        security = @SecurityRequirement(
+                name = "Security Token"
+        )
+)
+@SecurityScheme(
+        name = "Security Token",
+        description = "Access Token For My API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {}
